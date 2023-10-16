39fa6d8296ba5a038ca14b1d6451687f9c065dd3
ChinoManda <francofedullo22@gmail.com>
2023-10-16 10:09:49 -0300

    Validator

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\user;
 use Chat;
+use Illuminate\Support\Facades\Validator;
 class ChatController extends Controller
 {
     public function user(request $request){
@@ -33,22 +34,40 @@ class ChatController extends Controller
         return $conversation;
     }
 
+    public function MessageValidation(request $request){    
+        $validation = Validator::make($request->all(),[
+            'text'=>'required | string',           
+        ]);
+        return $validation;
+}
+
     public function SendMessage(request $request){
         $user = self::user($request);
         $conversation = self::ListOneConversation($request);
-        if ($conversation){
-          if (self::CheckUserGroup($user, $conversation)){
-            $text = $request->post("text");
-            $message = Chat::message($text)
-                ->from($user)
-                ->to($conversation)
-                ->send();
-            return $message;
-          }
-          return "User is not part of this conversation";
+        if (!$conversation){
+            return "Conversation does not exist";
         }
-        return "Conversation does not exist";
+        if (!self::CheckUserGroup($user, $conversation)){
+            return "User is not part of this conversation";
+        } 
+
+        $validation = self::MessageValidation($request);
+        if ($validation->fails()){
+            return $validation->errors();
+        }
+        $text = $request->post("text");   
+        return self::SendMessageRequest($text, $user, $conversation);
     }
+    
+    public function SendMessageRequest($text, $user, $conversation){ 
+        $message = Chat::message($text)
+            ->from($user)
+            ->to($conversation)
+            ->send();
+ 
+            return response($message, 201);
+    }
+
 /*
     public function ChatData(request $request, $conversation){
         $data = ['name' => $request->post("name"), 'description' => $request->post("description")];
