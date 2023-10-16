ae9b9e7d5c52c0f68d7c4e3411e141fe0123b7bb
ChinoManda <francofedullo22@gmail.com>
2023-10-15 22:50:23 -0300

    Enviar mensaje con validacion

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -13,12 +13,41 @@ class ChatController extends Controller
         $user->fill($userData);
         return $user;
     }
-
+    public function CheckUserGroup($user, $conversation){
+        $conversations = Chat::conversations()->setParticipant($user)->get()->where('conversation_id', $conversation->id);
+        if(!$conversations->isEmpty()){
+        return true;
+        }
+        return false;
+    }
     public function CreateChat(request $request){
         $user = self::user($request);
         $participants = [$user];
         $conversation = Chat::createConversation($participants)->makePrivate(false);
         return $conversation;
+    }
+    
+    public function ListOneConversation(request $request){
+        $id = $request->post("id_chat");
+        $conversation = Chat::conversations()->getById($id); 
+        return $conversation;
+    }
+
+    public function SendMessage(request $request){
+        $user = self::user($request);
+        $conversation = self::ListOneConversation($request);
+        if ($conversation){
+          if (self::CheckUserGroup($user, $conversation)){
+            $text = $request->post("text");
+            $message = Chat::message($text)
+                ->from($user)
+                ->to($conversation)
+                ->send();
+            return $message;
+          }
+          return "User is not part of this conversation";
+        }
+        return "Conversation does not exist";
     }
 /*
     public function ChatData(request $request, $conversation){
