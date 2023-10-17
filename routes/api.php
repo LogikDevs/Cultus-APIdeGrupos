87169d3f5623427a62db832ff88f384f5f207982
ChinoManda <francofedullo22@gmail.com>
2023-10-17 20:08:11 -0300

    Ruta

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GroupsController;
 use App\Http\Controllers\ChatController;
+use App\Http\Controllers\IntegratesController;
 use App\Http\Middleware\Autenticacion;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -19,4 +20,5 @@ Route::prefix('v1')->middleware(Autenticacion::class)->group(function(){
 
     Route::post("/message", [ChatController::class, "SendMessage"]);
     Route::get("/chats", [GroupsController::class, "ListUserGroups"]);
+    Route::get("/integrates/{d}", [IntegratesController::class, "ListGroupIntegrates"]);
 });
