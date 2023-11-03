b311ab91f399149935a5e9ce3be64442f52a2531
ChinoManda <francofedullo22@gmail.com>
2023-11-03 10:16:37 -0300

    Function expell, con validacion, request y transacciones

--- app/Http/Controllers/IntegratesController.php
+++ app/Http/Controllers/IntegratesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\integrates;
 use App\Models\user;
+use App\Models\groups;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 class IntegratesController extends Controller
@@ -70,8 +71,8 @@ class IntegratesController extends Controller
         return $Integrates;
     }
 
-    public function UserIntegrate($User, $Id){
-        $Integrate = integrates::get()->where('id_user', $User->id)->where('id_group', $Id)->first();
+    public function UserIntegrate($id_user, $id_group){
+        $Integrate = integrates::get()->where('id_user', $id_user)->where('id_group', $id_group)->first();
         return $Integrate;
     }
 
@@ -83,4 +84,79 @@ class IntegratesController extends Controller
         })->all();
         return $users;
     }
+
+    public function ValidateAdmin($User, $Id){
+        $Integrate = self::UserIntegrate($User->id, $Id);
+        if ($Integrate == null)
+        return response()->json("User is not part of the group", 405);
+
+        return($Integrate->rol == "Admin");
+    }
+
+
+    public function ExpellUserValidation(request $request){
+        $validation = Validator::make($request->all(),[
+            'id_group'=> 'required | exists:groups,id_group',
+            'id_user'=> 'required | exists:users,id',
+        ]);
+        return $validation;
+    }
+
+    public function ExpelUser(request $request, $Id){
+
+        $validation = self::ExpellUserValidation($request);
+        if ($validation->fails())
+        return $validation->errors();
+
+        $expelled = $request->post("id_user");
+        
+        $id_group = $request->post("id_group"); 
+        $User = self::GetUser($request);
+    
+        if ($expelled == $User->id)
+        return response()->json("Users can't expel themselves", 405);
+
+        $UserGroup = self::UserIntegrate($User->id, $id_group);
+        if ($UserGroup == null)
+        return response()->json("User is not part of the group", 405);
+
+        if (self::ValidateAdmin($User, $id_group) == false)
+        return response()->json("User is not admin", 405);
+
+        
+        $Integrate = self::UserIntegrate($expelled, $id_group);
+        if ($Integrate == null)
+        return response()->json("Expelled user is not part of the group", 405);
+        
+        
+
+        $Group = new Groups();      
+        $Group = $Group::findOrFail($id_group);
+        $id_chat = $Group->id_chat;        
+
+        return response (self::ExpelUserRequest($expelled, $id_chat, $Integrate), 201);
+    }
+
+    public function ExpelUserRequest($expelled, $id_chat, $Integrate){
+        $chatController = new ChatController();
+        
+        try {
+            DB::raw('LOCK TABLE integrates WRITE');
+            DB::beginTransaction();
+            
+            $Integrate->delete();
+            $chatController->ExpelUser($id_chat, $expelled);
+            DB::commit();
+            DB::raw('UNLOCK TABLES');
+            return ("User expelled");
+        }
+        catch (\Illuminate\Database\QueryException $th) {
+            DB::rollback();
+            return $th->getMessage();
+        }
+        catch (\PDOException $th) {
+            return response("Permission to DB denied",403);
+        }
+    }    
+
 }
